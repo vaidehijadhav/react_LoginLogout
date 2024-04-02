8432815b1f8b4e3f10ec76841e89bc3fba44de40
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-04-02 17:29:49 +0530

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import { Route, Routes } from "react-router-dom";
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="login" element={<Login/>}/>
+        <Route path="signup" element={<Signup/>}/>
+        <Route path="dashboard" element={<Dashboard/>}/>
       </Routes>
     </div>
   )
