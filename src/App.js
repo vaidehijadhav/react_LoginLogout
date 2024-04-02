998a75eb13ad4ded5daa13f06839c86146cb8048
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-04-02 17:26:18 +0530

    import navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
