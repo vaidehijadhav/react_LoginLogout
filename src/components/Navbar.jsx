aba5574c3e22f4b6b88ede41302607abaac256dd
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-04-03 18:50:30 +0530

    create navbar and buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,56 @@ import logo from '../assets/Logo.svg';
 
 const Navbar = ()=>{
     return(
-        <div>
+        <div className='flex'>
 
             <Link to="/">
                 <img src={logo} alt='Logo' width={160} height={32} loading='lazy' />
             </Link>
+
+            <nav >
+                <ul className='flex gap-3'>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/'>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className='flex ml-5 gap-3'>
+                {
+                    <Link to="/login">
+                        <button>
+                            Login
+                        </button>
+                    </Link>
+                }
+                {
+                    <Link to="/signup">
+                        <button>
+                            Signup
+                        </button>
+                    </Link>
+                }
+                {
+                    <Link to="/">
+                        <button>
+                            Logout
+                        </button>
+                    </Link>
+                }
+                {
+                    <Link to="/Dashboard">
+                        <button>
+                            Dashboard
+                        </button>
+                    </Link>
+                }
+            </div>
         </div>
     )
 }
